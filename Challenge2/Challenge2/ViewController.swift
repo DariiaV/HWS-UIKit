c9c34fdbefdabf6381fa5acaa05eea3b00f3f479
Dariia <dariia.grigoreva@gmail.com>
2022-12-20 21:03:19 +0400

    complete challenge 2

---
+++ Challenge2/Challenge2/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Challenge2
+//
+//  Created by Дария Григорьева on 20.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let tableView = UITableView()
+    private let cellReuseIdentifier = "cell"
+    private var shoppingList = ["Apple", "Milk", "Orange juice"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+        setupNavigationItem()
+    }
+    
+    private func setupNavigationItem() {
+        title = "Shopping List"
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let rightItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewItem))
+        rightItem.tintColor = .white
+        navigationItem.rightBarButtonItem = rightItem
+        let leftItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareShoppingList))
+        leftItem.tintColor = .white
+        navigationItem.leftBarButtonItem = leftItem
+        
+        
+        
+    }
+    
+    private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.frame = view.frame
+        tableView.backgroundColor = UIColor(hexString: "F0DBDB")
+    }
+    
+    @objc private func addNewItem() {
+        let alertVC = UIAlertController(title: "Add New Item", message: nil, preferredStyle: .alert)
+        alertVC.addTextField { textField in
+            textField.placeholder = "Create new item"
+        }
+        let action = UIAlertAction(title: "Add Item", style: .default) { [self] _ in
+            if let text = alertVC.textFields?.first?.text, !text.isEmpty {
+                let newItem = String(text)
+                shoppingList.append(newItem)
+                
+                self.tableView.insertRows(at: [IndexPath(row: self.shoppingList.count - 1, section: 0)], with: .automatic)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        alertVC.addAction(action)
+        alertVC.addAction(cancelAction)
+        present(alertVC, animated: true)
+    }
+    
+    @objc private func shareShoppingList() {
+        let shoppingList = shoppingList.joined(separator: "\n")
+        let vc = UIActivityViewController(activityItems: ["Shopping list:", shoppingList], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc,animated: true)
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier, for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        cell.backgroundColor = UIColor(hexString: "F0DBDB")
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            shoppingList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
